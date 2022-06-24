af5465b5117483bb216ff443891630257e964ad1
Jan Oliver Oelerich <janoliver@oelerich.org>
2022-06-24 10:43:11 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='cascading-shell',
-    version='1.0',
+    version='1.1',
     packages=['cash'],
     license='GPL',
     author="Jan Oliver Oelerich",
